04065aa7e7c2249e17ff193ace0d57a56bcc2902
eaxusniddinovich <eaxusniddinovich@gmail.com>
2024-01-30 12:56:52 +0500

    Add project files.

---
+++ CalculatorApplication/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorApplication;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    double prevNum = 0;
+    string operation = "";
+    bool newNumber = true;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private TextBox txtResult;
+    private void OpenParenthesisButton_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+
+    private void D1Button_Click(object sender, RoutedEventArgs e)
+    {
+       
+    }
+}
